19f4f9104de9844b72a49c539ced4b180481b761
Larsson Omberg <lom@larssono.com>
2023-04-18 07:36:13 -0700

    Moved code to make it easier to parallelize by iterating over assessmentId

--- mtb_scorer.py
+++ mtb_scorer.py
@@ -64,20 +64,21 @@ def studyEntityIds(syn, studyId):
     return entDict
 
 
-def load_data(syn, project_id):
+def load_data(syn, project_id, filters=None):
     """ Loading parquet data into pandas dataframe
     
     Args:
         syn: Synapse object
         project_id: parquet file synID
-        
+        filters: list of filters passed to load Parquet function
+
     Return:
         data frames df_metadata, df_stepdata, df_task_data
     """
-    df_metadata = gsyn.parquet_2_df(syn, project_id, 'dataset_archivemetadata_v1/')
-    df_stepdata = gsyn.parquet_2_df(syn, project_id, 'dataset_sharedschema_v1_steps/')
-    df_task_data = gsyn.parquet_2_df(syn, project_id, 'dataset_sharedschema_v1/')
-    df_task_status = gsyn.parquet_2_df(syn, project_id, 'dataset_sharedschema_v1_taskStatus/')
+    df_metadata = gsyn.parquet_2_df(syn, project_id, 'dataset_archivemetadata_v1/', filters)
+    df_stepdata = gsyn.parquet_2_df(syn, project_id, 'dataset_sharedschema_v1_steps/', filters)
+    df_task_data = gsyn.parquet_2_df(syn, project_id, 'dataset_sharedschema_v1/', filters)
+    df_task_status = gsyn.parquet_2_df(syn, project_id, 'dataset_sharedschema_v1_taskStatus/', filters)
     
     #Merge task status
     df_task_status = df_task_status.drop(columns=['id', 'index'])
@@ -106,36 +107,6 @@ def get_studyreference(syn, table_id):
     return study_reference
 
 
-def compute_scores(df_metadata, df_stepdata, df_task_data, study_df, filename):
-    """Calculating MTB score
-    
-    Args:
-        meta_info: meta info pandas dataframe
-        study_df: participant info
-        filename: filename info
-        
-    Return:
-        Combined processed score for each task
-    """
-    spelling_score = cs.get_score(df_task_data, df_stepdata, df_metadata, 'spelling', study_df) #Spelling
-    vocab_score = cs.get_score(df_task_data, df_stepdata, df_metadata, 'vocabulary', study_df) #vocabulary
-    psm_score = cs.get_score(df_task_data, df_stepdata, df_metadata, 'psm', study_df) #psm
-    
-    score_mfs = cs.get_score(df_task_data, df_stepdata, df_metadata, 'memory-for-sequences', study_df) #mfs
-    score_dccs = cs.get_score(df_task_data, df_stepdata, df_metadata, 'dccs', study_df) #dccs
-    score_fname = cs.get_score(df_task_data, df_stepdata, df_metadata, 'fnameb', study_df) #fname
-    
-    score_nm = cs.get_score(df_task_data, df_stepdata, df_metadata, 'number-match', study_df) #Number Match
-    score_flanker = cs.get_score(df_task_data, df_stepdata, df_metadata, 'flanker', study_df) #Flanker
-    
-    stack_merged = cs.combine_scores([score_mfs, score_dccs, score_fname, score_nm, score_flanker, psm_score, spelling_score,
-                                      vocab_score], df_metadata)
-    
-    if not os.path.exists(home_path): #create directory if not available
-        os.mkdir(home_path)
-        
-    stack_merged.to_csv(filename, index=False)
-    return stack_merged
 
 def upload_score(syn, file_path, git_path, des_synid):
     """ Upload final score to Syanpse
@@ -164,16 +135,7 @@ def clean_score(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
 
-def process_scores(args, config):
-    """ Processing and uploading MTB scores
     
-    Args:
-        args: input arguments
-        config: configuration object
-        
-    """
-    return 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process MTB score......")
     parser.add_argument("--auth_token", help="Authentication Token", required=True)
@@ -181,26 +143,28 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     syn = synapseclient.login(authToken = args.auth_token)
-    ## How to access authentication toward Synapse using env variables
-    #import json, os, synapseclient
-    # secrets=json.loads(os.getenv("SCHEDULED_JOB_SECRETS"))
-    # auth_token = secrets["PAT"]
-    # syn=synapseclient.Synapse()
-    # syn.login(authToken=auth_token)
 
-    #Get information about studies in Synapse
+    # Get information about studies in Synapse
     studies = getStudies(syn)
     studies = studies.merge(pd.DataFrame([studyEntityIds(syn, id) for id in studies.id]), left_on='id', right_on='projectId')
 
-    for i, study in studies.iterrows():
-        #process_scores(args, studies)
+    # Create storage location of output files
+    if not os.path.exists(home_path): #create directory if not available
+         os.mkdir(home_path)
 
-        file_path = os.path.join(home_path, 'MTB_' + study['studyId'] + '_scores.csv')
-        df_metadata, df_stepdata, df_task_data = load_data(syn, study['parquetFolderId'])
+    for i, study in studies.iterrows():
+        logger.info(study['studyId']+' '+study['name'])
         study_df = get_studyreference(syn, study['participantVersionsId'])
 
-        compute_scores(df_metadata, df_stepdata, df_task_data, study_df,  file_path)
-        upload_score(syn, file_path, args.git, config['dest_id'][key])
-        clean_score(file_path)#cleaning processed score
-    
-        
+        scores = []
+        for assessmentId in ['dccs', 'spelling','vocabulary', 'psm','memory-for-sequences', 'fnameb', 'number-match', 'flanker']:
+            print(assessmentId)
+            filter = [('assessmentid', '=', assessmentId)]
+            df_metadata, df_stepdata, df_task_data = load_data(syn, study['parquetFolderId'], filter)
+            scores.append(cs.get_score(df_task_data, df_stepdata, df_metadata, assessmentId, study_df))
+        stack_merged = cs.combine_scores(scores, df_metadata).sort_values('startDate')
+        stack_merged.to_csv(os.path.join(home_path, 'MTB_' + study['studyId'] + '_scores.csv'), index=False)
+
+        #upload_score(syn, file_path, args.git, config['dest_id'][key])
+        #clean_score(file_path)#cleaning processed score
+
